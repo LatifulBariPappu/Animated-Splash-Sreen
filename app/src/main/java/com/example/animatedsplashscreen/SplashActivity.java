02900d0d8c19854aca063689ce3289cf195e2576
LatifulBari <latifulbari2001@gmail.com>
2023-06-15 00:28:09 +0600

    Second animation

--- app/src/main/java/com/example/animatedsplashscreen/SplashActivity.java
+++ app/src/main/java/com/example/animatedsplashscreen/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.animatedsplashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread td=new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent =new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        td.start();
+
     }
 }
