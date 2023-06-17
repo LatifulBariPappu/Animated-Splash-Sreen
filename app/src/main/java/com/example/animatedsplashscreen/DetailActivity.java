e145a6a5328bd078c0655f081e5b795e0cf16d35
LatifulBari <latifulbari2001@gmail.com>
2023-06-17 22:43:30 +0600

    Third animation

--- app/src/main/java/com/example/animatedsplashscreen/DetailActivity.java
+++ app/src/main/java/com/example/animatedsplashscreen/DetailActivity.java
@@ -3,12 +3,21 @@ package com.example.animatedsplashscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    LottieAnimationView lottieAnimationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        lottieAnimationView=findViewById(R.id.covid_animation);
+        lottieAnimationView.setVisibility(View.VISIBLE);
+        lottieAnimationView.playAnimation();
+
     }
 }
